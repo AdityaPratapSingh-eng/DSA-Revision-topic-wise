1f38e0966ee2dbe7e508884ce833efd8f526a62b
Aditya Pratap Singh <adityasingh84610510@gmail.com>
2026-03-26 23:39:25 +0530

    top-down approach

---
+++ DP Series/House_robber-ll.cpp
@@ -0,0 +1,43 @@
+ #include <bits/stdc++.h>
+using namespace std;
+
+             int t[101];
+          int solve(vector<int>& nums, int i, int n){
+
+            if(i>n)return 0;
+
+            if(t[i]!=-1)return t[i];
+
+            int take=  nums[i] + solve(nums, i+2, n);
+            int skip= solve(nums, i+1, n);
+
+            return t[i]= max(take, skip);
+          }
+          // top- down approachh (recursion + memoization)
+    
+    int rob(vector<int>& nums) {
+          int n= nums.size();
+
+          if(n==1)return nums[0];
+          if(n==2)return max(nums[0], nums[1]);
+          memset(t, -1, sizeof(t));
+          // case-1   take 0th:
+         int take= solve(nums, 0, n-2);
+
+         memset(t, -1, sizeof(t));
+         // case -2  skip 0th
+         int skip= solve(nums, 1, n-1);
+
+         return max(take, skip );
+    }
+        int main(){
+            int n;
+            cin>>n;
+            vector<int>nums(n);
+            for(int i=0; i<n; i++){
+                cin>>nums[i];
+            }
+
+            cout<<rob(nums);
+            return 0;
+        }   
