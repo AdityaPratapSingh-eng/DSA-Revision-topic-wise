623a4d7568e9d5b4496832c25e3f6beab750ec27
Aditya Pratap Singh <adityasingh84610510@gmail.com>
2026-09-17 01:19:15 +0530

    Time: 210 ms (27.98%), Space: 17.7 MB (47.1%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,32 +1,10 @@
 class Solution {
-public:
-
-            int n;
-
-            vector<vector<bool>>isPal;
-       int solve(string s, int k, int i, int j, vector<vector<int>>&t){
-            for(int i=n-1; i>=0; i--){
-                for(int j=n-1; j>=0; j--){
-                    if(isPal[i][j]){
-                        int take= 1+ ((j+k<=n)?t[j+1][j+k]:0);
-                        int grow= t[i][j+1];
-                        int slide= t[i+1][j+1];
-
-                        t[i][j]= max({take, grow, slide});
-                    }
-
-                    int grow= t[i][j+1];
-                    int slide= t[i+1][j+1];
-
-                    t[i][j]= max({t[i][j], grow, slide});
-                }
-            }
-            return t[0][k-1];
-       }
+public:    
+   vector<vector<bool>>isPal;
     int maxPalindromes(string s, int k) {
-         n= s.size();
-         if(k==1)return n;
-         isPal.assign(n+1, vector<bool>(n+1, false));
+        int n= s.size();
+          if(k==1)return n;
+          isPal.assign(n+1, vector<bool>(n+1, false));
           for(int L= 1; L<=n; L++){
               for(int i=0; i+L<=n; i++){
                    int j= i+L-1;
@@ -43,9 +21,22 @@ public:
               }
         
           }
-         vector<vector<int>>t(n+1, vector<int>(n+1));
+                 vector<int>t(n+1,-1);
+          for(int len=0; len<k; len++){
+              t[len]=0;
+          }
+          for(int len=k; len<=n; len++){
+             int result= t[len-1];
 
-         return solve(s, k, 0 , k-1, t);
+             int j= len-1;
+             for(int i=0; j-i+1>=k; i++){
+                if(isPal[i][j]){
+                 result= max(result, 1+ t[i]);
+                }
+             }
+             t[len]= result;
+          }
 
+          return t[n];
     }
 };
