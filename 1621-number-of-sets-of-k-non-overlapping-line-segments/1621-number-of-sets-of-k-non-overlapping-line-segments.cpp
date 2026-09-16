21b981de552933268d4a68fc696bcd5633d2ef87
Aditya Pratap Singh <adityasingh84610510@gmail.com>
2026-09-17 02:19:17 +0530

    Time: 51 ms (54.98%), Space: 34.7 MB (38.39%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,21 +1,25 @@
 class Solution {
-public:
-    int numberOfSets(int n, int k) {
-        const int MOD = 1000000007;
-          vector<int> dp(n), prefixSums(n + 1);
-        for (int j =0; j <n; j++) {
-            dp[j]= 1;
-            prefixSums[j + 1]= (prefixSums[j]+dp[j]) % MOD;
-        }
-        for (int i =1; i<=k; i++) {
-            dp[0]= 0;
-            for (int j=1; j<n; j++) {
-                dp[j]= (dp[j - 1] + prefixSums[j]) % MOD;
+public:   
+     int M= 1e9+7;
+     int t[1001][1001];
+    int numberOfSets(int n, int K) {
+           for(int i=0; i<n; i++){
+            t[0][i]= 1;
+           }
+
+           for(int k=1;  k<=K; k++){
+              vector<int>prevSum(n+1, 0);
+              for(int x=n-1; x>=0; x--){
+                  prevSum[x]= (prevSum[x+1]+ t[k-1][x])%M;
+              }
+            for(int i=n-1; i>=0; i--){
+               
+                long long take= prevSum[i+1];
+                long long skip = t[k][i+1];
+                t[k][i]= (take+ skip)%M;
             }
-            for (int j = 0; j < n; j++) {
-                prefixSums[j + 1] = (prefixSums[j] + dp[j]) % MOD;
-            }
-        }
-        return dp[n - 1];
+           }
+
+           return t[K][0];
     }
 };
