789bfbad554a6989ef2e499971bafdfe865b123b
Aditya Pratap Singh <adityasingh84610510@gmail.com>
2026-09-17 00:44:29 +0530

    Time: 836 ms (10.71%), Space: 216.6 MB (14.29%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,49 +1,51 @@
-class Solution { 
-      
-public:           
-          
-               
-               bool isPal(string &s, int i, int j){
-                     while(i<j){
-                         if(s[i]!= s[j])return false;
+class Solution {
+public:
 
-                         i++;
-                         j--;
-                     }
+            int n;
 
-                     return true;
-               }
-       int solve(string &s, int k , int i, int j, vector<vector<int>>&t){
-            if(i>=s.size() || j>=s.size()){
-                return 0;
+            vector<vector<bool>>isPal;
+       int solve(string s, int k, int i, int j, vector<vector<int>>&t){
+            for(int i=n-1; i>=0; i--){
+                for(int j=n-1; j>=0; j--){
+                    if(isPal[i][j]){
+                        int take= 1+ ((j+k<=n)?t[j+1][j+k]:0);
+                        int grow= t[i][j+1];
+                        int slide= t[i+1][j+1];
 
+                        t[i][j]= max({take, grow, slide});
+                    }
+
+                    int grow= t[i][j+1];
+                    int slide= t[i+1][j+1];
+
+                    t[i][j]= max({t[i][j], grow, slide});
+                }
             }
-               if(t[i][j]!= -1){
-                return t[i][j];
-               }
-
-            if(isPal(s, i, j)){
-                 int take= 1+ solve(s, k , j+1, j+k, t);
-
-                 int grow= solve(s, k, i, j+1,t);
-
-                 int slide= solve(s, k, i+1, j+1,t);
-
-                 return t[i][j]=  max({take, grow, slide});
-            }
-
-                    int grow= solve(s, k, i, j+1,t);
-
-                    int slide= solve(s, k, i+1, j+1, t);
-
-                    return t[i][j]=  max(grow, slide);
-            
-             
+            return t[0][k-1];
        }
     int maxPalindromes(string s, int k) {
-        int n= s.size();
-        if(k==1)return n;
-        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
-        return solve(s, k, 0, k-1,dp);
+         n= s.size();
+         if(k==1)return n;
+         isPal.assign(n+1, vector<bool>(n+1, false));
+          for(int L= 1; L<=n; L++){
+              for(int i=0; i+L<=n; i++){
+                   int j= i+L-1;
+
+                   if(i==j){
+                        isPal[i][j]= true;
+                   }
+                   else if(i+1==j){
+                      isPal[i][j]= (s[i]==s[j])?true: false;
+                   }
+                   else{
+                     isPal[i][j]= ((s[i]==s[j]) && isPal[i+1][j-1]);
+                   }
+              }
+        
+          }
+         vector<vector<int>>t(n+1, vector<int>(n+1));
+
+         return solve(s, k, 0 , k-1, t);
+
     }
 };
